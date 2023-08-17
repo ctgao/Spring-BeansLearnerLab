987f30ba84d0844a004df0dd66afa011741ebc5e
Christine <christine@zipcodersMBP13.localdomain>
2023-08-17 15:13:25 -0400

    not sure what we doin

---
+++ src/main/java/com/example/SpringBeansLearnerLab/models/Instructors.java
@@ -0,0 +1,7 @@
+package com.example.SpringBeansLearnerLab.models;
+
+public class Instructors extends People<Instructor>{
+    public Instructors(Instructor... instructors){
+        super(instructors);
+    }
+}
