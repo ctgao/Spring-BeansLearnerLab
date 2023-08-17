8b7296b9d258cc289916e64ce480a6e93e5c4d34
Christine <christine@zipcodersMBP13.localdomain>
2023-08-17 14:41:10 -0400

    step 0.0 is done

---
+++ src/main/java/com/example/SpringBeansLearnerLab/SpringBeansLearnerLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBeansLearnerLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeansLearnerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeansLearnerLabApplication.class, args);
+	}
+
+}
